84fed6336a45f74b658fc8bdf44c7b76e4d89f90
siddeshshewde <siddesh.shewde@gmail.com>
2021-11-12 22:22:53 -0500

    Test

--- Gryphon_Web_Crawler/settings.py
+++ Gryphon_Web_Crawler/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&uqt$0@o@%j$mzfgrz8m181c^gty_y+kkim&cx*$sbenjd%me@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'gryphon-py.herokuapp.com']
 
@@ -128,6 +128,9 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
