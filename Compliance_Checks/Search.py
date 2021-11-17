580a5b7d4ff78621562638dd97ba28926c329e8f
siddeshshewde <siddesh.shewde@gmail.com>
2021-11-17 02:56:30 -0500

    Added check for duplicate IDs on more than one element.

--- Compliance_Checks/Search.py
+++ Compliance_Checks/Search.py
@@ -1,10 +1,12 @@
 import requests
 from bs4 import BeautifulSoup 
+import collections
 
 def meta_description_tag_check(url):
     '''
     Checking for meta tag description.
         - A well-written meta description attracts more clicks in search results than an irrelevant or missing description.
+    Priority 3 - AAA (https://www.bing.com/webmasters/help/webmasters-guidelines-30fba23a)
     @input:
         url: URL which is currently being crawled.
     @output:
@@ -24,6 +26,7 @@ def meta_description_tag_check(url):
 def title_tag_check(page_text):
     '''
     Checking for title on a web page.
+    Priority 1 - A (https://www.w3.org/TR/WCAG21/#page-titled)
     @input:
         page_text: Source code of the current web page.
     @output:
@@ -36,3 +39,21 @@ def title_tag_check(page_text):
     if title:
         return True
     return False
+
+def duplicate_id_check(page_text):
+    '''
+    Checking if same ID is used on more than one element.
+    Priority 1 - A (https://www.w3.org/TR/2008/REC-WCAG20-20081211/#ensure-compat-parses)
+    @input:
+        page_text: Source code of the current web page.
+    @output:
+        list: List of duplicate ids.
+    '''
+    soup = BeautifulSoup(page_text)
+
+    ids = [a.attrs['id'] for a in soup.find_all(attrs={'id': True})]
+    ids = collections.Counter(ids)
+
+    duplicate_ids = [key for key, value in ids.items() if value > 1]
+
+    return duplicate_ids
